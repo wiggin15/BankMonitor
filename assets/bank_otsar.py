e14ab8bff1fa3de2e936173b079b92d915582d05
Arnon Yaari <arnony@infinidat.com>
2015-11-25 19:28:09 +0200

    rewrite BankOtsar
    the old code still works but uses the older interface
    which may be obsolete one day
    (and also, the old interface has some ssl problems)

--- assets/bank_otsar.py
+++ assets/bank_otsar.py
@@ -4,39 +4,30 @@ from collections import OrderedDict
 from common import AssetBase, format_value
 
 class BankOtsar(AssetBase):
-    LOGIN_URL = "https://www.fibi-online.co.il/web/otsar"
-    HOME_URL = "https://www.fibi-online.co.il/web/TotalPosition.do?SUGBAKA=811&I-OSH-FLAG=1&I-PACHAK-FLAG=1&I-HODAOT-FLAG=1"
-    KRANOT_URL = "https://www.fibi-online.co.il/web/Processing?SUGBAKA=021"
-    VALUE_RE = """<td nowrap="yes">[0-9/]+</td><td nowrap="yes">(-?[0-9.,]+)</td><td nowrap="yes" dir="(?:rtl)?"><a href="\\./Processing\\?SUGBAKA=%s">"""
+    LOGIN_URL = "https://online.bankotsar.co.il/LoginServices/login2.do"
+    HOME_URL = "https://online.bankotsar.co.il/wps/myportal/FibiMenu/Online"
+    STOCK_URL = "https://online.bankotsar.co.il/wps/myportal/FibiMenu/Online/OnCapitalMarket/OnMyportfolio/AuthSecuritiesPrtfMyPFEquities"
 
     def _establish_session(self, username, password):
         s = requests.Session()
-        post_data = {'requestId': 'logon', 'ZIHMIST': username, 'KOD': password}
+        post_data = {"bankId": "OTSARPRTAL", "lang": "HE", "username": username, "password": password}
         s.post(self.LOGIN_URL, data=post_data)
         return s
 
-    def _get_bank_value(self, bank_data, bank_code, print_name=None):
-        val_matchobj = re.search(self.VALUE_RE % (bank_code,), bank_data)
-        if val_matchobj is None:
-            return 0
-        val = val_matchobj.group(1)
-        return format_value(val, print_name)
-
     def _get_values_from_main_page(self):
         main_page_html = self._session.get(self.HOME_URL).text
-        bank = self._get_bank_value(main_page_html, "077", "OSH")
-        deposit = self._get_bank_value(main_page_html, "101", "PIK")
-        car = self._get_bank_value(main_page_html, "200", "CAR")
-        return bank, deposit, car
+        OSH = re.search("current_balance[^>]+>\s*\S+\s*([^<]+)\s*", main_page_html)
+        OSH = OSH.group(1)
+        return format_value(OSH, 'OSH')
 
     def _get_stock_value(self):
-        stock_html = self._session.get(self.KRANOT_URL).text
-        NIA = re.search("<td class=\"TITLE\" nowrap>\s*([0-9,.]+)\s*</td>", stock_html)
-        NIA = NIA.group(1)
+        stock_html = self._session.get(self.STOCK_URL).text
+        NIA = re.findall("subtotal_val[^>]+>\s*\S+\s*([^<]+)\s*", stock_html)
+        NIA = NIA[-1]
         return format_value(NIA, 'NIA')
 
     def get_values(self):
-        bank, deposit, car = self._get_values_from_main_page()
+        bank = self._get_values_from_main_page()
         stock = self._get_stock_value()
-        return OrderedDict([("Bank", bank), ("Deposit", deposit), ("Stock", stock), ("Car", car)])
+        return OrderedDict([("Bank", bank), ("Deposit", 0), ("Stock", stock), ("Car", 0)])
 
