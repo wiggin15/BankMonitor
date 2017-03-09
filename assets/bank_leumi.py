ce8ee575d988c6da15b96262d98eb57310e8ded8
Avihai Berkovitz <aciduck@gmail.com>
2017-03-09 22:54:18 +0200

    Added deposit data

--- assets/bank_leumi.py
+++ assets/bank_leumi.py
@@ -10,6 +10,7 @@ class BankLeumi(AssetBase):
     CHECKING_RE = '<lblCurrentBalanceVal>(.+?)</lblCurrentBalanceVal>'
     HOLDINGS_URL = "https://hb2.bankleumi.co.il/uniquesig0/Trade/net/trade/portf/portfviews3.aspx"
     HOLDINGS_RE = '<td nowrap="nowrap" class="positive">&#8362 (.+?)</td>'
+    DEPOSIT_RE = '<pSavingsPos>(.+?)</pSavingsPos>'
 
     def _establish_session(self, username, password):
         s = requests.Session()
@@ -30,7 +31,16 @@ class BankLeumi(AssetBase):
         val = val_matchobj.group(1)
         return format_value(val, 'holdings')
 
+    def _get_deposit_balance(self):
+        summery_page = self._session.get(self.HOME_URL).text
+        val_matchobj = re.search(self.DEPOSIT_RE, summery_page)
+        if val_matchobj is None:
+            return 0
+        val = val_matchobj.group(1).decode('base64')[4:]
+        return format_value(val, 'deposit')
+
     def get_values(self):
         return OrderedDict([("Checking", self._get_checking_balance()),
-                            ("Holdings", self._get_holdings_balance())])
+                            ("Holdings", self._get_holdings_balance()),
+                            ("Deposit", self._get_deposit_balance())])
 
