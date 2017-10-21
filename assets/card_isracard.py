edd07c1a47b00c9ec3a3f2760f06ebd12da5c33e
Avihai Berkovitz <aciduck@gmail.com>
2017-10-21 20:12:09 +0300

    Fixed Isracard asset

--- assets/card_isracard.py
+++ assets/card_isracard.py
@@ -1,39 +1,57 @@
-import re
+import json
 import requests
-from common import CardBase, format_value
+from common import CardBase
 
 
 class CardIsracard(CardBase):
-    ISRACARD_PAGE = "https://service.isracard.co.il/isracard/isracard"
-    TRANSACTION_ID = "RikuzChiuvimAtidiim_202"
+    LOGIN_URL = "https://digital.isracard.co.il/personalarea/Login/"
+    LOGIN_VALIDATE_URL = "https://digital.isracard.co.il/services/ProxyRequestHandler.ashx?reqName=ValidateIdData"
+    LOGIN_POST_URL = "https://digital.isracard.co.il/services/ProxyRequestHandler.ashx?reqName=performLogonI"
+    CARD_DATA_URL = "https://digital.isracard.co.il/services/ProxyRequestHandler.ashx?reqName=DashboardCharges&format=Json&cardIdx=&returnDataStructureLevel=1&cardIndexes=&accountNumber=&actionCode=0&identityId="
+
+    def __init__(self, asset_section, user_id=None, card_suffix=None, **asset_options):
+        self.__user_id = user_id
+        self.__card_suffix = card_suffix
+        if not self.__user_id or not self.__card_suffix:
+            raise Exception("{} user ID or card suffix are missing".format(asset_section.capitalize()))
+        super(CardIsracard, self).__init__(asset_section, user="dummy", **asset_options)
 
     def _establish_session(self, username, password):
-        user_id, user_name = username.split(",", 1)
-        data = dict(reqName="performLogonI",
-                    MisparZihuy=user_id,
-                    KodMishtamesh=user_name,
-                    Sisma=password)
+        headers = {"content-type": "application/x-www-form-urlencoded; charset=UTF-8"}
         s = requests.Session()
-        s.post(self.ISRACARD_PAGE, data=data)
+        s.get(self.LOGIN_URL)
+
+        data = {
+            "id": self.__user_id,
+            "cardSuffix": self.__card_suffix,
+            "checkLevel": "1",
+            "companyCode": "11",
+            "idType": "1",
+            "countryCode": "212",
+        }
+        post_data_str = json.dumps(data)
+        result_str = s.post(self.LOGIN_VALIDATE_URL, data=post_data_str, headers=headers).text
+        result = json.loads(result_str)
+        assert result["Header"]["Status"] == "1"
+
+        data = {
+            "MisparZihuy": self.__user_id,
+            "cardSuffix": self.__card_suffix,
+            "idType": "1",
+            "countryCode": "212",
+            "Sisma": password,
+        }
+        post_data_str = json.dumps(data)
+        result_str = s.post(self.LOGIN_POST_URL, data=post_data_str, headers=headers).text
+        result = json.loads(result_str)
+        assert result["status"] == "1"
         return s
 
-    def _get_card_value(self, card_index):
-        data = dict(reqName="action",
-                    transactionId=self.TRANSACTION_ID,
-                    csrfToken=self._csrf_token,
-                    transactionType="by_card",
-                    CardIndex=str(card_index))
-        card_sum_page = self._session.post(self.ISRACARD_PAGE, data=data).text
-        value_text = re.findall('DetailsTD">\s*(\S+)\s*</td>', card_sum_page)[-1]
-        return format_value(value_text)
-
     def get_credit(self):
-        sum_page = self._session.get("{}?reqName={}".format(self.ISRACARD_PAGE, self.TRANSACTION_ID)).text
-        num_cards_text = re.search('<option.*?value="(\d+)"\s*>[^<]+</option>\s*</select>', sum_page).group(1)
-        num_cards = int(num_cards_text) + 1
-        self._csrf_token = re.search('csrfToken" value="([^"]+)"', sum_page).group(1)
-        card_total = sum(self._get_card_value(card_index) for card_index in range(num_cards))
-        return 0 - card_total
+        card_data_raw = self._session.get(self.CARD_DATA_URL).text
+        card_data = json.loads(card_data_raw)
+        upcoming_billing = card_data["DashboardChargesBean"]["inOut"][0]["nextTotalsInOut"][0]["billingSumSekelInOut"]
+        return 0 - upcoming_billing
 
     def get_next(self):
         return 0
